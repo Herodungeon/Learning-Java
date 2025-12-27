9cb787d013df9f3b522b57c6cecd74305f4b5e23
Brian Burgess <brian.samuel.burgess@gmail.com>
2025-12-27 15:26:51 -0700

    L2datatypes.java

---
+++ src/L2datatypes.java
@@ -0,0 +1,31 @@
+public class L2datatypes {
+    public static void main(String[] args) {
+        System.out.println("Data types being assigned and printed:");
+
+        int intnum = 10;
+        byte bytenum = 20;
+        short shortnum = 30;
+        long longnum = 40;
+
+        float floatnum = 50.675f;
+        double doublenum = 50.1122334455d;
+
+        boolean booleannum = true;
+
+        char charnum = 'a';
+        String stringnum = "An entire string";
+
+        System.out.println(
+                "Integer number: " + intnum + "\n" +
+                "Byte number: " + bytenum + "\n" +
+                "Short number: " + shortnum + "\n" +
+                "Long number: " + longnum + "\n" +
+                "Float number: " + floatnum + "\n" +
+                "Double number: " + doublenum + "\n" +
+                "Boolean value: " +  booleannum + "\n" +
+                "Character value: " +  charnum + "\n" +
+                "String value: " +  stringnum + "\n"
+        );
+
+        }
+    }
