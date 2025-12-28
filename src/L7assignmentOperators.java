cd00b3287beeb0b1fc78d3d8e4c12781ef9e9e23
Brian Burgess <brian.samuel.burgess@gmail.com>
2025-12-28 12:06:35 -0700

    Lesson 7 done, different ways to initialize variables

---
+++ src/L7assignmentOperators.java
@@ -0,0 +1,24 @@
+public class L7assignmentOperators {
+    public static void main(String[] args) {
+
+        int x = 100;
+        int y = 20;
+
+        x += y;
+        System.out.println("x = " + x);
+        x -= y;
+        System.out.println("x = " + x);
+        x *= y;
+        System.out.println("x = " + x);
+        x /= y;
+        System.out.println("x = " + x);
+        x %= y;
+        System.out.println("x = " + x);
+
+        x++;
+        System.out.println("x = " + x);
+
+        x--;
+        System.out.println("x = " + x);
+    }
+}
