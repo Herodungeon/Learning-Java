91b6484cf84507e38d1ade01e640d76aaf5343b5
Brian Burgess <brian.samuel.burgess@gmail.com>
2025-12-28 10:33:24 -0700

    Finished lesson three, mostly playing around with comments and used a string variable

---
+++ src/L3readingcode.java
@@ -0,0 +1,18 @@
+public class L3readingcode {
+    public static void main(String[] args) {
+        System.out.println("Playing around with some content from lesson 3.");
+
+        /* Testing comments
+        * Testing long comments
+        * And now declaring and assigning value to a variable (x)
+        * */
+        int x;
+        x = 10;
+        System.out.println("What x is initialized as: " + x);
+
+        // Initializing a String variable, and then assigning the length of the string to the integer value
+        String s = "Pizza and burgers";
+        x = s.length();
+        System.out.println("x after finding the length of the string 'Pizza and burgers': " + x);
+    }
+}
